adb0a31ba219eb0975b543bdc0db30ea14fc9967
Kip Schetnan <kipdougschetnan@gmail.com>
2022-12-07 17:10:20 -0600

    setting up jawsDB

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
       dialectOptions: {
         decimalNumbers: true,
       },
